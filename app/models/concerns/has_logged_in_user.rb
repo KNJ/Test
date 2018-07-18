8db562902d306492feb1d61a891aaf3993531bd7
kassy916 <warallyinfo@gmail.com>
2018-07-18 14:06:31 +0900

    delete models/concern

--- app/models/concerns/has_logged_in_user.rb
+++
@@ -1,9 +0,0 @@
-module HasLoggedInUser < Application
-  extend ActiveSupport::Concern
-
-  # ユーザーIDを保存
-  def logged_in?
-  	if user_signed_in?
- 	event_change_histories.user_id = current_user.user_id
-  end
-end
